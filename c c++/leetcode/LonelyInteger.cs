871a0196c497453fbfbae02cd9295bc80b3da32f
Bruno Matias Báez Medina <79769690+brunobaezmed@users.noreply.github.com>
2022-05-14 02:40:07 -0400

    Add files via upload

---
+++ c c++/leetcode/LonelyInteger.cs
@@ -0,0 +1,76 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<int> a)
+    {
+        int n = 0;
+        a.Sort();
+        int b = 0;
+        int c = 0;
+        int actual = a.ElementAt(0);
+        a.ForEach(r ->{
+        
+            //1 1 2 2 3 3 4 4 5 6 6 7 7
+        });
+        for(int q = 0 ; q < a.Count ; q++){
+          
+                
+              for( b = q+1 ; b < a.Count -1  && a.ElementAt(q) == a.ElementAt(b) ; b++ ){
+                  if(a.ElementAt(q) == a.ElementAt(b)){
+                      c++;
+                      q = b;
+                  }
+                  
+                
+              }
+              
+              if( c == 0 ){
+                  return a.ElementAt(q);
+                  
+              }else{ 
+                  c = 0;
+              }
+              
+        }
+        return 0;
+    }
+}
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.lonelyinteger(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
